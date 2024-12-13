d53a25a578639a520da4eb1fcbed7a6d850079da
Amy Jung <ajwjung@outlook.com>
2024-12-12 18:30:50 -0800

    feat: Users can open message in new page to view details

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,50 @@ const indexRouter = Router();
 
 const messages = [
     {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
+        id: 0,
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
     },
     {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
+        id: 1,
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
     }
 ];
 
-indexRouter.get("/", (req, res) => {
+indexRouter.get("/", (req, res, next) => {
     res.render("index", { title: "Mini Message Board", messages });
 });
 
-indexRouter.get("/new", (req, res) => {
+indexRouter.get("/new", (req, res, next) => {
     res.render("form", { title: "New Message" });
 });
 
-indexRouter.post("/new", (req, res) => {
+indexRouter.post("/new", (req, res, next) => {
     const data = req.body;
 
-    messages.push({ text: data.messageText, user: data.userName, added: new Date() });
+    messages.push({
+        id: messages.length,
+        text: data.messageText,
+        user: data.userName,
+        added: new Date()
+    });
 
     res.redirect("/");
-})
+});
+
+indexRouter.get("/message/:messageId", (req, res, next) => {
+    const messageId = req.params.messageId;
+    
+    const message = messages.find((message) => message.id === parseInt(messageId));
+    
+    if (!message) {
+        return res.status("404").send("Message not found");
+    };
+
+    res.render("message", { title: "User Message", message });
+});
 
 module.exports = indexRouter;
