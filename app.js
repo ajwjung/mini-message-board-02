49ae1f5491560d09c9d60a1e23e68157cd038201
Amy Jung <ajwjung@outlook.com>
2024-12-13 15:43:19 -0800

    feat: Add error handling middleware for consistent error responses

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.statusCode || 500).send(err.message || "Something went wrong");
+});
 
 const PORT = process.env.PORT || 3000;
 
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,33 +1,34 @@
 const db = require("../db");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
+const asyncHandler = require("express-async-handler");
 
-async function renderMessages(req, res, next) {
+const renderMessages = asyncHandler(async (req, res, next) => {
     const allMessages = await db.getAllMessages();
     res.render("index", { title: "Mini Message Board", messages: allMessages });
-};
+});
 
-async function renderForm(req, res, next) {
+const renderForm = asyncHandler(async (req, res, next) => {
     res.render("form", { title: "New Message" });
-};
+});
 
-async function postMessageContents(req, res, next) {
+const postMessageContents = asyncHandler(async (req, res, next) => {
     const data = req.body;
 
-    db.addNewMessage(data.messageText, data.userName);
+    await db.addNewMessage(data.messageText, data.userName);
 
     res.redirect("/");
-};
+});
 
-async function getMessageById(req, res, next) {
+const getMessageById = asyncHandler(async (req, res, next) => {
     const messageId = req.params.messageId;
     
     const message = await db.getMessageById(parseInt(messageId));
     
     if (!message) {
-        res.status("404").send("Message not found");
-        return;
+        throw new CustomNotFoundError("Message not found");
     };
 
     res.render("message", { title: "User Message", message });
-};
+});
 
 module.exports = { renderMessages, renderForm, postMessageContents, getMessageById };
